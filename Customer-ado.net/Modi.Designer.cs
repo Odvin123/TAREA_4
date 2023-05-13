7954abc1be8ce8ee5fd60b874d363f0d2d48476a
Ricardo Salvador Garcia Cruz <ricardo.garcia93u@std.uni.edu.ni>
2023-05-12 20:09:22 -0600

    Entrega

---
+++ Customer-ado.net/Modi.Designer.cs
@@ -0,0 +1,446 @@
+﻿namespace Customer_ado.net
+{
+    partial class Modi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dgvCustomer = new DataGridView();
+            txtNameCompany = new TextBox();
+            txtContactName = new TextBox();
+            txtContactTitle = new TextBox();
+            txtAddress = new TextBox();
+            txtCity = new TextBox();
+            txtRegion = new TextBox();
+            txtCodePostal = new TextBox();
+            txtCountry = new TextBox();
+            txtPhone = new TextBox();
+            txt_Fax = new TextBox();
+            lblCustomID = new Label();
+            btnCrear = new Button();
+            btnActualizar = new Button();
+            button2 = new Button();
+            btnLimpiartxts = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            bttonsalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvCustomer).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(1033, 375);
+            label11.Margin = new Padding(4, 0, 4, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(38, 24);
+            label11.TabIndex = 32;
+            label11.Text = "Fax";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(1029, 308);
+            label10.Margin = new Padding(4, 0, 4, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(77, 24);
+            label10.TabIndex = 31;
+            label10.Text = "Telefono";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(1033, 243);
+            label9.Margin = new Padding(4, 0, 4, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(43, 24);
+            label9.TabIndex = 30;
+            label9.Text = "País";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(1038, 178);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(119, 24);
+            label8.TabIndex = 29;
+            label8.Text = "Codigo Postal";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(1038, 111);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 24);
+            label7.TabIndex = 28;
+            label7.Text = "Region";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(271, 303);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 24);
+            label6.TabIndex = 27;
+            label6.Text = "Ciudad";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(271, 246);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 24);
+            label5.TabIndex = 26;
+            label5.Text = "Dirreccion";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(271, 190);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(152, 24);
+            label4.TabIndex = 25;
+            label4.Text = "Titulo de Contacto";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(271, 136);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(170, 24);
+            label3.TabIndex = 24;
+            label3.Text = "Nombre de Contacto";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(271, 80);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(193, 24);
+            label2.TabIndex = 23;
+            label2.Text = "Nombre de la compañia";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1152, 43);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(219, 25);
+            label1.TabIndex = 22;
+            label1.Text = "__Customer Record Data__";
+            // 
+            // dgvCustomer
+            // 
+            dgvCustomer.AllowUserToAddRows = false;
+            dgvCustomer.AllowUserToDeleteRows = false;
+            dgvCustomer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvCustomer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCustomer.Dock = DockStyle.Bottom;
+            dgvCustomer.Location = new Point(0, 460);
+            dgvCustomer.Margin = new Padding(4, 5, 4, 5);
+            dgvCustomer.Name = "dgvCustomer";
+            dgvCustomer.ReadOnly = true;
+            dgvCustomer.RowHeadersWidth = 62;
+            dgvCustomer.RowTemplate.Height = 25;
+            dgvCustomer.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvCustomer.Size = new Size(1513, 395);
+            dgvCustomer.TabIndex = 0;
+            dgvCustomer.CellClick += dgvCustomer_CellClick;
+            // 
+            // txtNameCompany
+            // 
+            txtNameCompany.Location = new Point(510, 73);
+            txtNameCompany.Margin = new Padding(4, 5, 4, 5);
+            txtNameCompany.Name = "txtNameCompany";
+            txtNameCompany.Size = new Size(265, 31);
+            txtNameCompany.TabIndex = 34;
+            // 
+            // txtContactName
+            // 
+            txtContactName.Location = new Point(510, 129);
+            txtContactName.Margin = new Padding(4, 5, 4, 5);
+            txtContactName.Name = "txtContactName";
+            txtContactName.Size = new Size(265, 31);
+            txtContactName.TabIndex = 35;
+            // 
+            // txtContactTitle
+            // 
+            txtContactTitle.Location = new Point(510, 178);
+            txtContactTitle.Margin = new Padding(4, 5, 4, 5);
+            txtContactTitle.Name = "txtContactTitle";
+            txtContactTitle.Size = new Size(265, 31);
+            txtContactTitle.TabIndex = 36;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(510, 239);
+            txtAddress.Margin = new Padding(4, 5, 4, 5);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(265, 31);
+            txtAddress.TabIndex = 37;
+            // 
+            // txtCity
+            // 
+            txtCity.Location = new Point(510, 299);
+            txtCity.Margin = new Padding(4, 5, 4, 5);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(265, 31);
+            txtCity.TabIndex = 38;
+            // 
+            // txtRegion
+            // 
+            txtRegion.Location = new Point(1185, 100);
+            txtRegion.Margin = new Padding(4, 5, 4, 5);
+            txtRegion.Name = "txtRegion";
+            txtRegion.Size = new Size(265, 31);
+            txtRegion.TabIndex = 39;
+            // 
+            // txtCodePostal
+            // 
+            txtCodePostal.Location = new Point(1185, 166);
+            txtCodePostal.Margin = new Padding(4, 5, 4, 5);
+            txtCodePostal.Name = "txtCodePostal";
+            txtCodePostal.Size = new Size(265, 31);
+            txtCodePostal.TabIndex = 40;
+            // 
+            // txtCountry
+            // 
+            txtCountry.Location = new Point(1185, 231);
+            txtCountry.Margin = new Padding(4, 5, 4, 5);
+            txtCountry.Name = "txtCountry";
+            txtCountry.Size = new Size(265, 31);
+            txtCountry.TabIndex = 41;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(1185, 296);
+            txtPhone.Margin = new Padding(4, 5, 4, 5);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(265, 31);
+            txtPhone.TabIndex = 42;
+            // 
+            // txt_Fax
+            // 
+            txt_Fax.Location = new Point(1185, 363);
+            txt_Fax.Margin = new Padding(4, 5, 4, 5);
+            txt_Fax.Name = "txt_Fax";
+            txt_Fax.Size = new Size(265, 31);
+            txt_Fax.TabIndex = 43;
+            // 
+            // lblCustomID
+            // 
+            lblCustomID.AutoSize = true;
+            lblCustomID.Location = new Point(40, 40);
+            lblCustomID.Margin = new Padding(4, 0, 4, 0);
+            lblCustomID.Name = "lblCustomID";
+            lblCustomID.Size = new Size(114, 25);
+            lblCustomID.TabIndex = 44;
+            lblCustomID.Text = "ID del cliente";
+            // 
+            // btnCrear
+            // 
+            btnCrear.Location = new Point(4, 245);
+            btnCrear.Margin = new Padding(4, 5, 4, 5);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(193, 42);
+            btnCrear.TabIndex = 45;
+            btnCrear.Text = "Nuevo";
+            btnCrear.UseVisualStyleBackColor = true;
+            btnCrear.Click += btnCrear_Click;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Location = new Point(4, 103);
+            btnActualizar.Margin = new Padding(4, 5, 4, 5);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(193, 59);
+            btnActualizar.TabIndex = 46;
+            btnActualizar.Text = "Modificar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(4, 289);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 54);
+            button2.TabIndex = 47;
+            button2.Text = "Eliminar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // btnLimpiartxts
+            // 
+            btnLimpiartxts.Location = new Point(4, 177);
+            btnLimpiartxts.Margin = new Padding(4, 5, 4, 5);
+            btnLimpiartxts.Name = "btnLimpiartxts";
+            btnLimpiartxts.Size = new Size(193, 58);
+            btnLimpiartxts.TabIndex = 48;
+            btnLimpiartxts.Text = "Restablecer";
+            btnLimpiartxts.UseVisualStyleBackColor = true;
+            btnLimpiartxts.Click += btnLimpiartxts_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.MenuHighlight;
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1513, 40);
+            panel1.TabIndex = 49;
+            panel1.Paint += panel1_Paint;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.GradientActiveCaption;
+            panel2.Controls.Add(lblCustomID);
+            panel2.Controls.Add(bttonsalir);
+            panel2.Controls.Add(btnActualizar);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(btnCrear);
+            panel2.Controls.Add(btnLimpiartxts);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 40);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(201, 420);
+            panel2.TabIndex = 50;
+            // 
+            // bttonsalir
+            // 
+            bttonsalir.Location = new Point(4, 361);
+            bttonsalir.Name = "bttonsalir";
+            bttonsalir.Size = new Size(193, 56);
+            bttonsalir.TabIndex = 49;
+            bttonsalir.Text = "Cerrar";
+            bttonsalir.UseVisualStyleBackColor = true;
+            bttonsalir.Click += bttonsalir_Click;
+            // 
+            // Modi
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1513, 855);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(txt_Fax);
+            Controls.Add(txtPhone);
+            Controls.Add(txtCountry);
+            Controls.Add(txtCodePostal);
+            Controls.Add(txtRegion);
+            Controls.Add(txtCity);
+            Controls.Add(txtAddress);
+            Controls.Add(txtContactTitle);
+            Controls.Add(txtContactName);
+            Controls.Add(txtNameCompany);
+            Controls.Add(dgvCustomer);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Modi";
+            Text = "frmCustomer";
+            ((System.ComponentModel.ISupportInitialize)dgvCustomer).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DataGridView dgvCustomer;
+        private TextBox txtNameCompany;
+        private TextBox txtContactName;
+        private TextBox txtContactTitle;
+        private TextBox txtAddress;
+        private TextBox txtCity;
+        private TextBox txtRegion;
+        private TextBox txtCodePostal;
+        private TextBox txtCountry;
+        private TextBox txtPhone;
+        private TextBox txt_Fax;
+        private Label lblCustomID;
+        private Button btnCrear;
+        private Button btnActualizar;
+        private Button button2;
+        private Button btnLimpiartxts;
+        private Panel panel1;
+        private Panel panel2;
+        private Button bttonsalir;
+    }
+}
---
+++ Customer-ado.net/Modi.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Customer_ado.net
+{
+    
+    public partial class Modi : Form
+    {
+        string connectionString =
+            ConfigurationManager.
+            ConnectionStrings["Customer_ado.net.Propiedades.Settings1.con"].
+            ConnectionString;
+        public Modi()
+        {
+            InitializeComponent();
+            GetCustomer();
+        }
+
+        #region Metodos ("DAR DOBLE CLICK AQUI PARA ABRIR LOS METODOS")
+        private void GetCustomer()
+        {
+            string queryString = "SELECT CustomerID,CompanyName,ContactName,ContactTitle," +
+                "Address,City,Region,PostalCode,Country,Phone,Fax " +
+                 "FROM Customers ORDER BY ContactName;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dgvCustomer.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+        private string letras()
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var Charsarr = new char[4];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            var resultString = new String(Charsarr);
+            return resultString;
+        }
+        private void Limpiartxt()
+        {
+            lblCustomID.Text = "ID del cliemte";
+            txtNameCompany.Text = String.Empty;
+            txtContactName.Text = String.Empty;
+            txtContactTitle.Text = String.Empty;
+            txtAddress.Text = String.Empty;
+            txtCity.Text = String.Empty;
+            txtRegion.Text = String.Empty;
+            txtCodePostal.Text = String.Empty;
+            txtCountry.Text = String.Empty;
+            txtPhone.Text = String.Empty;
+            txt_Fax.Text = String.Empty;
+
+        }
+
+        private void IngresarCustomer()
+        {
+            string Letras_Azar = letras();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                try
+                {
+                    string queryString = "INSERT INTO Customers(CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax) " +
+                        "VALUES('" + Letras_Azar + "','" + txtNameCompany.Text + "','" + txtContactName.Text + "','" + txtContactTitle.Text + "','" +
+                                     txtAddress.Text + "', '" + txtCity.Text + "', '" + txtRegion.Text + "','" + txtCodePostal.Text + "','" + txtCountry.Text + "','"
+                                     + txtPhone.Text + "','" + txt_Fax.Text + "' )";
+
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Cliente Ingresado correctamente");
+                    GetCustomer();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void ActuCustomer()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string queryString = "UPDATE Customers SET CompanyName = '" + txtNameCompany.Text
+                        + "', ContactName = '" + txtContactName.Text + "',ContactTitle='" + txtContactTitle.Text
+                        + "',Address='" + txtAddress.Text + "',City='" + txtCity.Text + "',Region='" + txtRegion.Text
+                        + "',PostalCode='" + txtCodePostal.Text + "',Country='" + txtCountry.Text + "',Phone='"
+                        + txtPhone.Text + "',Fax='" + txt_Fax.Text + "' WHERE CustomerID = '" + lblCustomID.Text + "'";
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Cliente actualizado correctamente");
+                    GetCustomer();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void EliminarCustomers()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string queryString = "DELETE FROM Customers WHERE CustomerID = '" + lblCustomID.Text + "'";
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Cliente eliminado correctamente");
+                    GetCustomer();
+                }
+                catch (InvalidCastException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion
+
+
+        private void dgvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex >= 0)
+            {
+                // aqui necesito nuevas variables 
+                // cambialas y editalas 
+                // lo que esta en comillas no se cambian 
+                //cambia el textbox y label 
+                // en el form cambia la fuente de letra y movelas 
+                // me avisas por cualquier cosa
+                DataGridViewRow row = dgvCustomer.Rows[e.RowIndex];
+                lblCustomID.Text = row.Cells["CustomerID"].Value.ToString();
+                txtNameCompany.Text = row.Cells["CompanyName"].Value.ToString();
+                txtContactName.Text = row.Cells["ContactName"].Value.ToString();
+                txtContactTitle.Text = row.Cells["ContactTitle"].Value.ToString();
+                txtAddress.Text = row.Cells["Address"].Value.ToString();
+                txtCity.Text = row.Cells["City"].Value.ToString();
+                txtRegion.Text = row.Cells["Region"].Value.ToString();
+                txtCodePostal.Text = row.Cells["PostalCode"].Value.ToString();
+                txtCountry.Text = row.Cells["Country"].Value.ToString();
+                txtPhone.Text = row.Cells["Phone"].Value.ToString();
+                txt_Fax.Text = row.Cells["Fax"].Value.ToString();
+
+            }
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            IngresarCustomer();
+            Limpiartxt();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            ActuCustomer();
+            Limpiartxt();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EliminarCustomers();
+            Limpiartxt();
+        }
+
+        private void btnLimpiartxts_Click(object sender, EventArgs e)
+        {
+            Limpiartxt();
+        }
+
+        private void bttonsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //Aqui necesita una codigo para que el panel se pueda mover
+            // si podes busca un tuco de codigo que permita moverlo ʕ•́ᴥ•̀ʔっ♡
+
+
+            //ReleaseCapture();
+            //SendMessage(this.Handle, 0x112, 0xf012, 0);
+            // este no deja moverlo
+            // no es necesario duh
+        }
+
+
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
